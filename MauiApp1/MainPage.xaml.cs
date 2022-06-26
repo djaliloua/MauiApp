6a469791ca260431168cff5af140b75c99c63354
Abdou-Djalilou Ali <djaliloua@gmail.com>
2022-06-26 16:57:42 +0200

    Aggiungere i file di progetto.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,122 @@
+﻿namespace MauiApp1;
+using System.Data;
+using System.Text;
+using System.Text.RegularExpressions;
+
+public partial class MainPage : ContentPage
+{
+    List<char> Operators;
+    public MainPage()
+	{
+		InitializeComponent();
+        Operators = new List<char>();
+        Operators.Add('+');
+        Operators.Add('-');
+        Operators.Add('*');
+        Operators.Add('/');
+    }
+    private void Show(object sender, EventArgs e)
+    {
+
+    }
+	private void Write(object sender, EventArgs e)
+    {
+		var but = sender as Button;
+		if(entry.Text == "0")
+        {
+            entry.Text = but.Text;
+        }
+		
+        else
+            entry.Text += but.Text;
+        bntAC.Text = "C";
+    }
+	private void ButtonDel(object sender, EventArgs e)
+    {
+        
+        if(entry.Text.Length == 1)
+        {
+            entry.Text = "0";
+            bntAC.Text = "AC";
+            bntParenthesis.Text = "(";
+        }
+        else
+            entry.Text = entry.Text.Substring(0, entry.Text.Length - 1);
+    }
+	private void ButtonDot(object sender, EventArgs e)
+    {
+        bntAC.Text = "C";
+        entry.Text += ".";
+
+    }
+    
+    private void ButtonAC(object sender, EventArgs e)
+    {
+		entry.Text = "0";
+        bntAC.Text = "AC";
+        if(bntParenthesis.Text == ")")
+            bntParenthesis.Text = "(";
+    }
+    private void btnequal_Click(object sender, EventArgs e)
+    {
+        DataTable dt = new DataTable();
+        try
+        {
+            var v = dt.Compute(entry.Text, "");
+            if(v.ToString().Contains(','))
+            {
+                entry.Text = v.ToString().Replace(',', '.');
+            }
+            else
+                entry.Text = v.ToString();
+        }
+        catch(Exception ex)
+        {
+            entry.Text = ex.Message;
+        }
+    }
+    
+    private void ButtonPercentage(object sender, EventArgs e)
+    {
+        object[] percentage = Utility.IsParsable(entry.Text);
+        var value = (string)percentage[0];
+        var s = (int)percentage[1];
+        bool b = Operators.All(x => !entry.Text.Contains(x));
+        if (b)
+            try
+            {
+                entry.Text = (float.Parse(entry.Text) / 100).ToString().Replace(',', '.');
+            }
+            catch
+            {
+
+            }
+        else
+            entry.Text = String.Concat(entry.Text.Remove(s + 1), (float.Parse(value) / 100).ToString().Replace(',', '.'));
+    }
+    private void ParenthesisButton(object sender, EventArgs e)
+    {
+        bntAC.Text = "C";
+        if (bntParenthesis.Text == "(")
+        {
+            bntParenthesis.Text = ")";
+            if (entry.Text != "0")
+            {
+                entry.Text += "(";
+            }
+            else
+            {
+                entry.Text = "(";
+            }
+        }
+        else
+        {
+            bntParenthesis.Text = "(";
+            entry.Text += ")";
+        }
+
+    }
+
+	
+}
+
---
+++ MauiApp1/Utility.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MauiApp1
+{
+    internal class Utility
+    {
+        public static ConversionPercentage ApplyPercentage(string value)
+        {
+            ConversionPercentage percentage = new ConversionPercentage();
+
+            for (int i = 0; i < value.Length; i++)
+            {
+                try
+                {
+                    percentage.Value = (float.Parse(value.Substring(i, value.Length)) / 100).ToString().Replace(',', '.');
+                    break;
+                }
+                catch
+                {
+
+                }
+            }
+            return percentage;
+        }
+        public static object[] IsParsable(string value)
+        {
+            object[] result = new object[3];
+            string d = null;
+            int? startindex = null;
+            int r = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                try
+                {
+                    r = value.Length - i;
+                    d = float.Parse(value.Substring(i, r)).ToString();
+                    startindex = i;
+
+                    // Console.WriteLine(d);
+                    break;
+                }
+                catch
+                {
+
+                }
+            }
+            result[0] = d;
+            result[1] = startindex;
+            result[2] = r;
+            return result;
+        }
+        public static string LastValue(string value)
+        {
+            object[] result = new object[3];
+            string d = null;
+            int r = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                try
+                {
+                    r = value.Length - i;
+                    d = float.Parse(value.Substring(i, r)).ToString();
+                    
+
+                    // Console.WriteLine(d);
+                    break;
+                }
+                catch
+                {
+
+                }
+            }
+           
+            return d;
+        }
+        public static string RemainingValue(string value)
+        {
+            object[] result = new object[3];
+            string d = null;
+            int startindex = 0;
+            int r = 0;
+            for (int i = 0; i < value.Length; i++)
+            {
+                try
+                {
+                    r = value.Length - i;
+                    d = float.Parse(value.Substring(i, r)).ToString();
+                    startindex = i;
+
+                    // Console.WriteLine(d);
+                    break;
+                }
+                catch
+                {
+
+                }
+            }
+            
+            return value.Remove(startindex, d.Length);
+        }
+    }
+    
+    public class ConversionPercentage
+    {
+        public string Value { get; set; }
+    }
+}
